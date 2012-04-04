bfe2daafa5ee96a78295f116124aaa0c42229aa8
Harry Lee <leelee@fullsail.edu>
2012-04-04 02:31:45 -0400

    Add a local storage function.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,24 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;
 	}
 	
+	function storeData(){
+		var id 				= Math.floor(Math.random()*100000001);
+		//Gather up all our form field values and store in an object
+		//Object properties contain array with the form label and iput value.
+		var item					={};
+			item.bookl					=["Book Listed:", $('bookl').value];
+			item.authorn				=["Author Name:", $('authorn').value];
+			item.titleofbook			=["Title of Book:", $('titleofbook').value];
+			item.datee					=["Date Enter:", $('datee').value];
+			/*item.checkbox				=["Checkbox:", $('checkbox').value];*/
+			item.checksubjectofbook		=["Check Subject of Book:", $('checksubjectofbook').value];
+			item.ratethebook			=["Rate the Book:", ratethebook];
+			item.notes					=["Notes:", $('notes').value];
+		//Save data into Local Storage: Use Stringify to convert our object to a string.
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Date Saved!");
+	}
+	
 	
 	//Variable defaults
 	
