f6b49e1327acf180a66bc0b0f87408b60c0c78c0
Harry Lee <leelee@fullsail.edu>
2012-04-05 00:28:44 -0400

    Wrote the code for get data, store data and display data.

--- js/main.js
+++ js/main.js
@@ -10,23 +10,79 @@ window.addEventListener("DOMContentLoaded", function(){
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
+	function getSelectedCheckbox(){
+		var checkbox = document.forms(0).fiction;
+		for(var i=0; i<checkBox.length; i++){
+			if(checkBox.length[i].checked){
+				fiction = checkBox[i].value;
+			}
+		}
+	}
+	
+	function getCheckBoxValue(){
+		if($('fiction').checked){
+			fiction = $('fiction').value;
+		}else{
+			fiction = "No"
+		}
+	}
+	
+	function getCheckBoxValue(){
+		if($('nonfiction').checked){
+			nonfiction = $('nonfiction').value;
+		}else{
+			nonfiction = "No"
+		}
+	}
 	
 	function storeData(){
 		var id 				= Math.floor(Math.random()*100000001);
 		//Gather up all our form field values and store in an object
 		//Object properties contain array with the form label and iput value.
-		var item					={};
-			item.bookl					=["Book Listed:", $('bookl').value];
-			item.authorn				=["Author Name:", $('authorn').value];
-			item.titleofbook			=["Title of Book:", $('titleofbook').value];
-			item.datee					=["Date Enter:", $('datee').value];
-			/*item.checkbox				=["Checkbox:", $('checkbox').value];*/
-			item.checksubjectofbook		=["Check Subject of Book:", $('checksubjectofbook').value];
-			item.ratethebook			=["Rate the Book:", ratethebook];
-			item.notes					=["Notes:", $('notes').value];
+		getCheckBoxValue();
+		getFormValue();
+		var item						= {};
+			item.booklisted				= ["Book Listed:", $('bookllisted').value];
+			item.authorname				= ["Author Name:", $('authorname').value];
+			item.titleofbook			= ["Title of Book:", $('titleofbook').value];
+			item.dateenter				= ["Date Enter:", $('dateenter').value];
+			item.fiction				= ["Fiction:", fiction];
+			item.nonfiction				= ["Nonfiction:", nonfiction];
+			item.checksubjectofbook		= ["Check Subject of Book:", $('checksubjectofbook').value];
+			item.ratethebook			= ["Rate the Book:", $('ratethebook').value];
+			item.notes					= ["Notes:", $('notes').value];
 		//Save data into Local Storage: Use Stringify to convert our object to a string.
 		localStorage.setItem(id, JSON.stringify(item));
-		alert("Date Saved!");
+		alert("Save");
+	}
+	
+	function getData(){
+		//Write Date
+		var makeDiv = docment.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.creaeElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len;i++){
+			var makeli = document.createElement('li');
+			var linksli = document.createElement('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = locaStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = document.creatElement('ul');
+			makeli.appendChild(makeSubList);
+			getImage(obj.group[1], makeSubList);
+			for(var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linkLi); // Create our edit and delet buttons.*/
+		}
 	}
 	
 	
@@ -35,6 +91,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
 	function storeData(){
 		localStorage.setItem("test", "hello");
+		alert(localStorage.value(0));
 	}
 
 	//Sat Link & Submit Click Events
